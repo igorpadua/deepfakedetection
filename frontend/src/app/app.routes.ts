0ec6ddb494a65a87f880a07a5f0d675da1562c18
igorpadua <igorpadua@tutanota.com>
2024-06-13 12:27:01 -0300

    feat: adicionado rota principal

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {EnvioVideoImagemComponent} from "./paginas/envio-video-imagem/envio-video-imagem.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: EnvioVideoImagemComponent
+  }
+];
