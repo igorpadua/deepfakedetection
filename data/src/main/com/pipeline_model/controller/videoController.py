6b20bbd25cc8b67ae1aee9b3b483b982d5c8d755
igorpadua <igorpadua@tutanota.com>
2024-06-13 16:25:25 -0300

    Apenas o front end

--- data/src/main/com/pipeline_model/controller/videoController.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-def verificar_caminho(file_path):
-    cap = cv2.VideoCapture(file_path)
-    if not cap.isOpened():
-        return None, 'Não foi possível abrir o vídeo.'
-    return cap, None
