fb3e5e84542ddda99ba1a8efd3169374aa07d92b
igorpadua <igorpadua@tutanota.com>
2024-06-13 12:54:14 -0300

    feat: Tela de envio de video e imagem

--- frontend/src/app/paginas/envio-video-imagem/envio-video-imagem.component.ts
+++ frontend/src/app/paginas/envio-video-imagem/envio-video-imagem.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgbAccordionButton} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-envio-video-imagem',
   standalone: true,
-  imports: [],
+  imports: [
+    NgbAccordionButton
+  ],
   templateUrl: './envio-video-imagem.component.html',
   styleUrl: './envio-video-imagem.component.scss'
 })
